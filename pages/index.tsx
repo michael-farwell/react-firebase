ff71821998fa4420d876fed671a5a6c0e7956c06
Michael Farwell <mike@lie-nielsen.com>
2022-03-11 15:19:37 -0500

    add react-hot-toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import { toast } from "react-hot-toast";
+
 export default function Home () {
   return (
     <div>
+      <button onClick={ () => toast.success("Toast is Ready!") }>
+        Make Me Some Toast
+      </button>
     </div>
   );
 }
